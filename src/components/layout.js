edc1e7c8348769e0181b91a56b49bbd295eb8d19
kevinhtran <kevdeveloper7@gmail.com>
2020-08-27 17:09:21 -0700

    styled the main layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,56 +3,65 @@ import { Global, css } from '@emotion/core';
 
 const Layout = ({ children }) => (
   <>
-  <Global 
-    styles={css`
-      * {
-        box-sizing: border-box;
-        margin: 0;
-      }
-
-      * + * {
-        margin-top: 1rem;
-      }
-
-      html,
-      body {
-        margin: 0;
-        color: #555;
-        font-family: -apple-system, BlinkMacSystenFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
-        font-size: 18px;
-        line-height: 1.4;
-
-        /* remove margin for the main dix that Gatsby mounts into */
-        > div {
-          margin-top: 0;
+    <Global
+      styles={css`
+        * {
+          box-sizing: border-box;
+          margin: 0;
         }
 
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-          color: #222;
-          line-height: 1.1;
+        * + * {
+          margin-top: 1rem;
+        }
 
-          + * {
-            margin-top: 0.5rem;
+        html,
+        body {
+          margin: 0;
+          color: #555;
+          font-family: -apple-system, BlinkMacSystenFont, 'Segoe UI', Roboto,
+            Helvetica, Arial, sans-serif;
+          font-size: 18px;
+          line-height: 1.4;
+
+          /* remove margin for the main dix that Gatsby mounts into */
+          > div {
+            margin-top: 0;
+          }
+
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6 {
+            color: #222;
+            line-height: 1.1;
+
+            + * {
+              margin-top: 0.5rem;
+            }
+          }
+
+          strong {
+            color: #222;
+          }
+
+          li {
+            margin-top: 0.25rem;
           }
         }
-
-        strong {
-          color: #222;
-        }
-
-        li {
-          margin-top: 0.25rem;
-        }
-      }
-    `} 
+      `}
     />
     <header></header>
-    <main>{children}</main>
+    <main
+      css={css`
+        margin: 2rem auto 4rem;
+        max-width: 90vw;
+        width: 550px;
+      `}
+    >
+      {children}
+    </main>
   </>
 );
 
