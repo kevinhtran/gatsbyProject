c172f432ea0f17ed724e4ac1424f4fc94bda0f23
kevinhtran <kevdeveloper7@gmail.com>
2020-08-29 16:05:10 -0700

    updated mdx file and added contacts page

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,15 +4,16 @@ module.exports = {
     description:
       'A site we built together during a full-day Frontend Masters Gatsby Workshop.',
   },
-  plugins: ['gatsby-plugin-emotion', 
+  plugins: [
+    'gatsby-plugin-emotion',
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-mdx',
       options: {
         defaultLayouts: {
-          default: require.resolve('./src/components/layout.js')
-        }
-      }
-    }
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
   ],
 };
