133a076091c34d27a03f643c4b1b39f18d45f5af
kevinhtran <kevdeveloper7@gmail.com>
2020-08-29 16:33:00 -0700

    added a wave component

---
+++ src/components/wave.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react',
+import { css } from '@emotion/core';
+
+const Wave = () => {
+  const [waves, setWaves] = useState(0);
+  const label = `:wave:${waves} ${waves === 1 ? 'wave' : 'waves'}`
+
+  return (
+    <button 
+      css={css}`
+        background: rebeccapurple;
+        border: none;
+        color: white;
+        font-size: 1.25rem
+      `}
+      onClick={() => setWaves(waves + 1)}
+    > 
+      {label}
+    </button >
+  );
+};
+  
+export default Wave;
+
+
+// the goal of this component is to have somebody to be able to click a button and it'll tell them that they waved and we want to know how many times they've waved
