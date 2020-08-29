90464ebf7e81feb60f5cd0953daa6d70ecbb15df
kevinhtran <kevdeveloper7@gmail.com>
2020-08-29 16:00:47 -0700

    installed and created mdx file

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,15 @@ module.exports = {
     description:
       'A site we built together during a full-day Frontend Masters Gatsby Workshop.',
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-emotion', 
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js')
+        }
+      }
+    }
+  ],
 };
