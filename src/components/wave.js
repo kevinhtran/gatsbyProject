a72d2741e279e063bc4c58d68348613882eda4e5
kevinhtran <kevdeveloper7@gmail.com>
2020-08-31 01:11:13 -0700

    fixed typo in wave component and updated mdx plugin

--- src/components/wave.js
+++ src/components/wave.js
@@ -1,26 +1,24 @@
-import React, { useState } from 'react',
+import React, { useState } from 'react';
 import { css } from '@emotion/core';
 
 const Wave = () => {
   const [waves, setWaves] = useState(0);
-  const label = `:wave:${waves} ${waves === 1 ? 'wave' : 'waves'}`
+  const label = `wave${waves} ${waves === 1 ? 'wave' : 'waves'}`;
 
   return (
-    <button 
-      css={css}`
+    <button
+      css={css`
         background: rebeccapurple;
         border: none;
         color: white;
-
       `}
       onClick={() => setWaves(waves + 1)}
-    > 
+    >
       {label}
-    </button >
+    </button>
   );
 };
-  
-export default Wave;
 
+export default Wave;
 
 // the goal of this component is to have somebody to be able to click a button and it'll tell them that they waved and we want to know how many times they've waved
