d38dbbb753bdd148249e8339942ba6997c9bae85
kevinhtran <kevdeveloper7@gmail.com>
2020-09-01 04:13:24 -0700

    styled with css and used query filters to customize images

--- src/components/post-preview.js
+++ src/components/post-preview.js
@@ -8,7 +8,8 @@ const PostPreview = ({ post }) => (
   <article
     css={css`
       border-bottom: 1px solid #ddd;
-      margin-top: 0.75rem;
+      display: flex;
+      margin-top: 0;
       padding-bottom: 1rem;
 
       :first-of-type {
@@ -20,11 +21,11 @@ const PostPreview = ({ post }) => (
       to={post.slug}
       css={css`
         margin: 1rem 1rem 0 0;
-        width: 100%;
+        width: 100px;
       `}
     >
       <Image
-        // fluid={???}
+        fluid={post.image.sharp.fluid}
         css={css`
           * {
             margin-top: 0;
@@ -33,11 +34,13 @@ const PostPreview = ({ post }) => (
         alt={post.title}
       />
     </Link>
-    <h3>
-      <Link to={post.slug}>{post.title}</Link>
-    </h3>
-    <p>{post.excerpt}</p>
-    <ReadLink to={post.slug}>read this post &rarr;</ReadLink>
+    <div>
+      <h3>
+        <Link to={post.slug}>{post.title}</Link>
+      </h3>
+      <p>{post.excerpt}</p>
+      <ReadLink to={post.slug}>read this post &rarr;</ReadLink>
+    </div>
   </article>
 );
 
