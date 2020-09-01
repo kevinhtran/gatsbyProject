3cc63cada5043b042df6c2084965ed47d7bb6bfc
kevinhtran <kevdeveloper7@gmail.com>
2020-09-01 02:01:50 -0700

    created hero image box for the home page with components and styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import Hero from '../components/hero';
 import PostPreview from '../components/post-preview';
 import usePosts from '../hooks/use-posts';
 
@@ -8,15 +9,14 @@ export default () => {
   const posts = usePosts();
 
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Hello World!</p>
-      <Link to="/about/">Learn about me &rarr;</Link>
-
-      <h2>Read my blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   );
 };
