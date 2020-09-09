c18ec7a877d6423229e32be906136c245550c781
kevinhtran <kevdeveloper7@gmail.com>
2020-09-08 23:55:42 -0700

    styled instagram component

--- src/components/insta.js
+++ src/components/insta.js
@@ -5,13 +5,50 @@ import useInstagram from '../hooks/use-instagram';
 
 const Insta = () => {
   const instaPhotos = useInstagram();
-  const { username } = instaPhotos[0];
+  const { kevdeveloper } = instaPhotos[0];
 
   return (
     <>
       <h2>Instagram posts from @kevdeveloper</h2>
       <div>
-        {instaPhotos.map(photo => ())}
+        {instaPhotos.map(photo => (
+          <>
+            <h2>Instagram posts from @{kevdeveloper}</h2>
+            <div>
+              {instaPhotos.map(photo => (
+                <a
+                  key={photo.id}
+                  href={`https://www.instagram.com/p/B43n271gGYI/`}
+                  css={css`
+                    box-shadow: 0;
+                    display: block;
+                    margin: 0.5rem;
+                    max-width: 120px;
+                    transition: 200ms box-shadow linear;
+
+                    :focus,
+                    :hover {
+                      box-shadow: 0 2px 14px #22222244;
+                      z-index: 10;
+                    }
+                  `}
+                >
+                  <Image
+                    fluid={photo.fluid}
+                    alt={photo.caption}
+                    css={css`
+                      width: 100%;
+
+                      * {
+                        margin-top: 0;
+                      }
+                    `}
+                  />
+                </a>
+              ))}
+            </div>
+          </>
+        ))}
       </div>
       <a href={`https://instagram.com/kevdeveloper`}>
         See more on Instagram &rarr;
